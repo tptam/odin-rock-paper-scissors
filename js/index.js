d064dfef533ea49e4e6adb3c2221ffa0819728cc
tptam <99264397+tptam@users.noreply.github.com>
2024-06-26 02:07:38 +0900

    Move helper functions and const into playGame

--- js/index.js
+++ js/index.js
@@ -1,37 +1,8 @@
-const ROCK = "rock";
-const PAPER = "paper"
-const SCISSORS = "scissors"
-
-
-function getComputerChoice() {
-    switch (Math.floor(Math.random()*3)) {
-        case 0:
-            return ROCK;
-        case 1:
-            return PAPER;
-        case 2:
-            return SCISSORS;
-    }
-}
-
-
-function getHumanChoice() {
-    const input = prompt("rock, paper, scissors?", ROCK);
-    switch (input.toLowerCase()) {
-        case ROCK:
-            return ROCK;
-        case PAPER:
-            return PAPER;
-        case SCISSORS:
-            return SCISSORS;
-        default:
-            // May be changed later
-            return ROCK;
-    }
-}
-
-
 function playGame() {
+    const ROCK = "rock";
+    const PAPER = "paper"
+    const SCISSORS = "scissors"
+    
     let humanScore = 0;
     let computerScore = 0;
 
@@ -45,29 +16,76 @@ function playGame() {
     if (humanScore === computerScore) {
         message = "Tie! " + message;
     } else if (humanScore > computerScore) {
-        message = "Final winner is... YOU!" + message;
+        message = "You win!" + message;
     } else {
-        message = "Final winner is... Computer!" + message;
+        message = "You Lose!" + message;
     }
     console.log(message);
 
+
+    // Helper Functions
+
+    // Returns a randomly selected hand
+    function getComputerChoice() {
+        switch (Math.floor(Math.random()*3)) {
+            case 0:
+                return ROCK;
+            case 1:
+                return PAPER;
+            case 2:
+                return SCISSORS;
+        }
+    }
+    
+
+    // Ask player their hand and returns the value
+    function getHumanChoice() {
+        const input = prompt("rock, paper, scissors?", ROCK);
+        switch (input.toLowerCase()) {
+            case ROCK:
+                return ROCK;
+            case PAPER:
+                return PAPER;
+            case SCISSORS:
+                return SCISSORS;
+            default:
+                // May be changed later
+                return ROCK;
+        }
+    }
+
+
+    // Plays one round and disply the result
     function playRound (humanChoice, computerChoice) {
-        humanChoice = humanChoice.toLowerCase();
-        console.log(humanChoice, computerChoice);
-        let message;
+
+        // let message = `Your hand: ${getPropCase(humanChoice)}\n`;
+        // message += `Computer's hand: ${getPropCase(computerChoice)}\n`
+
+        let message = "";
+
         if (humanChoice === computerChoice) {
-            message = `Tie! ${humanChoice} is as strong as ${computerChoice}!`;
+            message += "Tie! "
+            message += `${getPropCase(humanChoice)} is as strong as ${getPropCase(computerChoice)}!`;
         } else if (
             humanChoice === ROCK && computerChoice === PAPER ||
             humanChoice === PAPER && computerChoice === ROCK ||
             humanChoice === SCISSORS && computerChoice === PAPER
         ) {
-            message = `You win! ${humanChoice} beats ${computerChoice}!`;
+            message += "You win! "
+            message += `${getPropCase(humanChoice)} beats ${getPropCase(computerChoice)}!`;
             humanScore++;
         } else {
-            message = `You Lose! ${computerChoice} beats ${humanChoice}!`;
+            message += "You Lose! "
+            message += `${getPropCase(computerChoice)} beats ${getPropCase(humanChoice)}!`;
             computerScore++;
         }
         console.log(message);
     }
+
+}
+
+
+// Returns proper-cased string
+function getPropCase(str) {
+    return str.at(0).toUpperCase() + str.slice(1).toLowerCase();
 }
